eca29dfb55972e2e6e503d5762963c2e33de80e4
JM33348 <jpdejesus1@student.hau.edu.ph>
2023-11-18 02:52:43 +0800

    gumana kana

--- Learn_Box/settings/__init__.py
+++ Learn_Box/settings/__init__.py
@@ -1,16 +1,6 @@
 from .base import *
-<<<<<<< HEAD
 # from .production import *
-
-try:
-     from .local import *
-except:
-    pass
-=======
-#from .production import *
-
 try:
     from .local import *
 except:
     pass
->>>>>>> 180b149a7b1a240421f3beac93c3cc4a2fb9a10f
