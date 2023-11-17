ec9c39452a8542a99f531495380540e747a93f84
JM33348 <jpdejesus1@student.hau.edu.ph>
2023-11-18 00:01:04 +0800

    news

--- Learn_Box/settings/local.py
+++ Learn_Box/settings/local.py
@@ -146,3 +146,13 @@ EMAIL_HOST_PASSWORD = 'qugelblcwfslpaky'
 DEFAULT_FROM_EMAIL = 'noreply<no_reply@domain.com>'
 
 BASE_URL = "http://127.0.0.1:8000"
+
+CORS_REPLACE_HTTPS_REFERER      = False
+HOST_SCHEME                     = "http://"
+SECURE_PROXY_SSL_HEADER         = None
+SECURE_SSL_REDIRECT             = False
+SESSION_COOKIE_SECURE           = False
+CSRF_COOKIE_SECURE              = False
+SECURE_HSTS_SECONDS             = None
+SECURE_HSTS_INCLUDE_SUBDOMAINS  = False
+SECURE_FRAME_DENY               = False
