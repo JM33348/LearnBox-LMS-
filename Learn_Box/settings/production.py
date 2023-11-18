c66b70464c3eeb631b10df75fda21ee17c9ba64e
JM33348 <jpdejesus1@student.hau.edu.ph>
2023-11-18 14:19:33 +0800

    Prod

--- Learn_Box/settings/production.py
+++ Learn_Box/settings/production.py
@@ -148,17 +148,17 @@ EMAIL_HOST_PASSWORD = 'qugelblcwfslpaky'
 
 DEFAULT_FROM_EMAIL = 'noreply<no_reply@domain.com>'
 
-BASE_URL = "http://127.0.0.1:8000"
-
-CORS_REPLACE_HTTPS_REFERER      = True
-HOST_SCHEME                     = "https://"
-SECURE_PROXY_SSL_HEADER         = ('HTTP_X_FORWARDED_PROTO', 'https')
-SECURE_SSL_REDIRECT             = True
-SESSION_COOKIE_SECURE           = True
-CSRF_COOKIE_SECURE              = True
-SECURE_HSTS_INCLUDE_SUBDOMAINS  = True
-SECURE_HSTS_SECONDS             = 1000000
-SECURE_FRAME_DENY               = True
+# BASE_URL = "http://127.0.0.1:8000"
+#
+# CORS_REPLACE_HTTPS_REFERER      = True
+# HOST_SCHEME                     = "https://"
+# SECURE_PROXY_SSL_HEADER         = ('HTTP_X_FORWARDED_PROTO', 'https')
+# SECURE_SSL_REDIRECT             = True
+# SESSION_COOKIE_SECURE           = True
+# CSRF_COOKIE_SECURE              = True
+# SECURE_HSTS_INCLUDE_SUBDOMAINS  = True
+# SECURE_HSTS_SECONDS             = 1000000
+# SECURE_FRAME_DENY               = True
 
 # AWS_GROUP_NAME = 'CPE_User_Group'
 # AWS_USERNAME = 'Zhuske'
